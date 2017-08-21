344541456797a02f96d36030cf3926b85544e036
Henrik <henkedanne@gmail.com>
2017-08-21 21:34:35 +0200

    setup gulp and postcss. Cleaned up stylesheets

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function() {
+	return gulp.src('./styles.css')
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('watch', function() {
+	watch('./*.css', function() {
+		gulp.start('styles');
+	});
+});
