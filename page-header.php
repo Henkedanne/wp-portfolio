1c24543e1d4a10ba06fb5812207659ca42325559
Henrik Danielsson <henkedanne@gmail.com>
2017-09-19 14:55:31 +0200

    page-header.php added

---
+++ page-header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php bloginfo('name'); ?></title>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?>>
+    <div class="main-grid">
+        <header class="page-header">   
+                <nav class="page-header__nav" role="navigation">
+                    <?php wp_nav_menu( array('theme_location' => 'primary_menu')); ?>
+                </nav> <!-- .header__nav ends! -->
+        </header>    
