bd558095de4e95c232de3fd7f1eb697d7978e415
Henrik Danielsson <henkedanne@gmail.com>
2017-09-20 21:39:02 +0200

    made a addClass function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ var box = document.querySelectorAll('.content-grid__box');
 var textBox = document.querySelector('.box__text');
 var titleHolder = document.querySelector('.content-grid__title');
 var mainGrid = document.querySelector('.content-grid');
-
+var pageTitle = document.querySelector('.page-content__title');
 
 function showTitle(e) {
 	titleHolder.innerHTML = e.target.innerText;
@@ -12,9 +12,16 @@ function hideTitle() {
 	titleHolder.innerHTML = '';
 }
 
+function addClass(className, el) {
+	el.classList.add(className);
+}
+
+
+
 box.forEach(function(boxEl) {
 	boxEl.addEventListener('mouseover', showTitle);
 	boxEl.addEventListener('pointerleave', hideTitle);
 });
 
+addClass('test', pageTitle);
 console.log('I\'m looking for internship!!! Get in touch: henrik(.)danilesson(@)hyperisland.se');
