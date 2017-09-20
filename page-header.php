c99c4b730bbbe503326fd78678b656807280621f
Henrik Danielsson <henkedanne@gmail.com>
2017-09-20 11:38:58 +0200

    fixed position on header, added a button class.

--- page-header.php
+++ page-header.php
@@ -11,9 +11,9 @@
     <div class="main-grid">
         <header class="page-header">
 
-            <div class="page-header__back">
-                <a href="<?php echo home_url(); ?>">Go back</a>
-            </div>
+            
+                <a class="page-header__back button button--black" href="<?php echo home_url(); ?>">Go back</a>
+            
             <nav class="page-header__nav" role="navigation">
                 <?php wp_nav_menu( array('theme_location' => 'primary_menu')); ?>
             </nav> <!-- .header__nav ends! -->
