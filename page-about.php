4a6352d14bc028cb4e2742f880af1f59be04fd9a
Henrik Danielsson <henkedanne@gmail.com>
2017-09-23 21:52:32 +0200

    started working on the about page

---
+++ page-about.php
@@ -0,0 +1,6 @@
+<?php get_template_part('page-header'); ?>
+    <div class="about-content">
+        <h1 class="about-content__title">Hi! I'm Henrik!</h1>
+        <p class="about-content__text">I'm currently a student at Hyper Island in Stockholm where I attend the Digital Media Creative Program. I love to make things on the web. I think it's a great platform for getting products or "thingymathings" out in public. And to actually be able to influence a user with my work is such a cool thing. <br> I'm currently looking for internship and I would like to find a space where I can grow my skills in web-development and design.</p>
+    </div>
+<?php get_footer(); ?>
