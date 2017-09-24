bb4958f6e6face781e5b41d319707437db672cdc
Henrik Danielsson <henkedanne@gmail.com>
2017-09-24 12:53:14 +0200

    fixed the logo not showing correctly. added versioning on style.css

--- header.php
+++ header.php
@@ -10,9 +10,9 @@
     <body <?php body_class(); ?>>
     <div class="main-grid">
         <header class="header">
-            <div class="logo__wrapper">
-                <h1 class="logo__wrapper__logo"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-                <p class="logo__wrapper__tagline"><?php bloginfo('description'); ?></p>
+            <div class="header__logo-wrapper">
+                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+                <p><?php bloginfo('description'); ?></p>
             </div>   
                 <nav class="header__nav" role="navigation">
                     <?php wp_nav_menu( array('theme_location' => 'primary_menu')); ?>
