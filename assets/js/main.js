2e8754eb4cc80e5d3c0441cf996f9f63944bbddc
Henrik Danielsson <henkedanne@gmail.com>
2017-09-29 10:25:21 +0200

    started on hiding the scrollbar.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 var box = document.querySelectorAll('.content-grid__box');
-var textBox = document.querySelector('.box__text');
 var titleHolder = document.querySelector('.content-grid__title');
-var mainGrid = document.querySelector('.content-grid');
+var lastScrollTop = 0;
+var header = document.querySelector('.page-header');
+var headerHeight = header.scrollHeight;
 
 
 function showTitle(e) {
@@ -12,9 +13,30 @@ function hideTitle() {
 	titleHolder.innerHTML = '';
 }
 
+function scrolling() {
+	
+	var scrollTop = document.body.scrollTop;
+	console.log(scrollTop);
+
+	if (scrollTop > lastScrollTop) {
+		header.classList.add('page-header--hide');
+	} else if (scrollTop < lastScrollTop) {
+		header.classList.remove('page-header--hide');
+	}
+	lastScrollTop = scrollTop;
+}
+
+function headerHide() {
+	x;
+}
+
 box.forEach(function(boxEl) {
 	boxEl.addEventListener('mouseover', showTitle);
 	boxEl.addEventListener('pointerleave', hideTitle);
 });
+
+window.onscroll = function (){
+	scrolling();
+};
 
 console.log('I\'m looking for internship!!! Get in touch: henrik(.)danielsson(@)hyperisland.se');
